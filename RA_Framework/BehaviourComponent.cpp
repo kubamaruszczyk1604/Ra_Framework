5e12663a1107a2e6cd103e10b3e26798bc31c9d2
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2019-02-25 19:50:22 +0000

    added build in shader code

--- RA_Framework/BehaviourComponent.cpp
+++ RA_Framework/BehaviourComponent.cpp
@@ -1,8 +1,6 @@
 #include "BehaviourComponent.h"
-
 namespace RA_FRAMEWORK
 {
-
 	BehaviourComponent::BehaviourComponent(const std::string& ID) :
 		Component(ID, ComponentType::BEHAVIOUR_COMPONENT),
 		m_StartCalled{ false }{}
